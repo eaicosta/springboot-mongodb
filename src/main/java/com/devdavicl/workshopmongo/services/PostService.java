1e06bdf7349d1dfe174d6824cfb6aad847d19459
Davi Costa <davicostadelira04@gmail.com>
2023-12-29 16:56:23 -0300

    Post - findById

---
+++ src/main/java/com/devdavicl/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.devdavicl.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devdavicl.workshopmongo.domain.Post;
+import com.devdavicl.workshopmongo.repository.PostRepository;
+import com.devdavicl.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+	
+}
