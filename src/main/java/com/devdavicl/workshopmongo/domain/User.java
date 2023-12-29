36dff2bd00d5d2468da0c2620699a36b869cae78
Davi Costa <davicostadelira04@gmail.com>
2023-12-29 16:20:12 -0300

    Referencing user posts

--- src/main/java/com/devdavicl/workshopmongo/domain/User.java
+++ src/main/java/com/devdavicl/workshopmongo/domain/User.java
@@ -1,12 +1,15 @@
 package com.devdavicl.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection="user")
+@Document(collection = "user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -15,6 +18,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -48,6 +54,10 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
 
 	@Override
 	public int hashCode() {
@@ -65,5 +75,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
